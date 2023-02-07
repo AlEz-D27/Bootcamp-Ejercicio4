d048173fbc2cec1f94c178e7369bd667e4c60827
AlEzDuarte <alfre27ezequiel@gmail.com>
2023-02-06 22:25:40 -0300

    Cuarto ejercicio de Bootcamp

---
+++ Ejercicio4.js
@@ -0,0 +1,18 @@
+let Nombre = "Ezequiel";
+let Apellido = "Duarte";
+let Estudiante = Nombre.concat(" ", Apellido);
+let EstudianteMayus = Estudiante.toUpperCase();
+let EstudianteMinus = Estudiante.toLowerCase();
+let Num_Letras = Estudiante.length;
+let First_letNomb = Nombre.charAt(0);
+let Last_letApe = Apellido.charAt(5);
+let Sin_Espacios = Estudiante.trim();
+let Boolean_Estu = Estudiante.includes("Ezequiel")
+
+console.log(Estudiante)
+console.log(EstudianteMayus)
+console.log(EstudianteMinus)
+console.log(Num_Letras)
+console.log(First_letNomb)
+console.log(Last_letApe)
+console.log(Boolean_Estu)
