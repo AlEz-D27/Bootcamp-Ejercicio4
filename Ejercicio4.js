19a638292121a6f75461ea54b5aa7efa0e111354
AlEzDuarte <alfre27ezequiel@gmail.com>
2023-02-06 23:08:45 -0300

    Correccion en la variable Sin_Espacios

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -6,7 +6,7 @@ let EstudianteMinus = Estudiante.toLowerCase();
 let Num_Letras = Estudiante.length;
 let First_letNomb = Nombre.charAt(0);
 let Last_letApe = Apellido.charAt(5);
-let Sin_Espacios = Estudiante.trim();
+let Sin_Espacios = Estudiante.replace(/ /g, "");
 let Boolean_Estu = Estudiante.includes("Ezequiel")
 
 console.log(Estudiante)
@@ -15,4 +15,5 @@ console.log(EstudianteMinus)
 console.log(Num_Letras)
 console.log(First_letNomb)
 console.log(Last_letApe)
+console.log(Sin_Espacios)
 console.log(Boolean_Estu)
